c370c961aada7c6b10c23bc4336f6356a1f202e3
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2025-05-05 14:07:53 -0400

    Create command.sh
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

---
+++ command.sh
@@ -0,0 +1,5 @@
+# this is the update for the onnxruntime-gpu==1.21.1 for 11.8
+# if you install pip install onnxruntime-gpu==1.21.1 it will install default cuda12 and cudnn9 you DONT WANT THAT!!!!
+# Copy and Paste in CMD.exe 
+
+pip install onnxruntime-gpu==1.21.1 --index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-11/pypi/simple/ --extra-index-url https://pypi.org/simple
